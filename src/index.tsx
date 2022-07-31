613a00fa63f9da1bc09d7771622b614c65701baa
alszczep <al.szczepura@gmail.com>
2022-07-31 16:41:02 +0200

    Add ga-code

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-G9MQK7EMWC")
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
