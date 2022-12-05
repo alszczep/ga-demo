8169bb0f916a4a5f260cff6c01f8c16fdd9607e9
alszczep <al.szczepura@gmail.com>
2022-12-05 20:42:05 +0100

    Add gtm

---
+++ src/components/Gtm.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from "react";
+
+const Gtm: FunctionComponent = () => {
+  return (
+    <div>
+      <h1>GTM</h1>
+      <button>test1</button>
+      <button id="test">test2</button>
+    </div>
+  );
+};
+
+export default Gtm;
